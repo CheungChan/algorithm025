ac622c40d9c874a1c51ae8bf2e4b7dad05ec1fe0
阿章 <1377699408@qq.com>
2021-04-06 00:29:38 +0800

    1

---
+++ week01/合并两个有序链表.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+__author__ = '陈章'
+__date__ = '2021/4/6 00:27'
+"""
+将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+
+ 
+
+示例 1：
+
+
+输入：l1 = [1,2,4], l2 = [1,3,4]
+输出：[1,1,2,3,4,4]
+示例 2：
+
+输入：l1 = [], l2 = []
+输出：[]
+示例 3：
+
+输入：l1 = [], l2 = [0]
+输出：[0]
+ 
+
+提示：
+
+两个链表的节点数目范围是 [0, 50]
+-100 <= Node.val <= 100
+l1 和 l2 均按 非递减顺序 排列
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+"""
+我用的遍历，看题解有遍历和递归两种。遍历和递归时间复杂度都是M+N, 遍历的空间复杂度是1，递归是M+N。尽量用循环遍历吧
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        l3 = head = ListNode()
+        while l1 and l2:
+            if l1.val < l2.val:
+                l3.next = l1
+                l1 = l1.next
+            else:
+                l3.next = l2
+                l2 = l2.next
+            l3 = l3.next
+        l3.next = l1 if l1 else l2
+        return head.next
